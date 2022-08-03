5e900fed1fe1d996e326b0145a387cb1642e5b42
Datafox-JCP <datafox@outlook.com>
2022-08-02 20:28:18 -0500

    - Adjustment

--- City Places/Helpers/Constants.swift
+++ City Places/Helpers/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static var apiKey = "QrWuKeKZaJ4FbNQRgKGIfF1gVU3sZAk3fz8tts33bcFRtzGhoTPM6Tkgmi5ghV7zrsZd1YIb4g-69g4-X10x1XHQc8V8Y6uYcnBpsoB6kocJK-r1DSx8X6iDGW3pYnYx"
+    static var apiKey = "You key here"
     static var apiUrl = "https://api.yelp.com/v3/businesses/search"
     static var restaurantKey = "restaurants"
     static var sightsKey = "arts"
